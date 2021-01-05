977bbe48bfcdea72783990cb5b00038bc955f944
Jill-Ann <jillannkay@gmail.com>
2021-01-05 19:01:05 +0100

    Add tests for add, subtract, divide

--- functions.test.js
+++ functions.test.js
@@ -1,10 +1,37 @@
-import { multiply } from './functions';
-// set-up for test
+import { add, multiply, subtract, divide, setOperator, setInput } from './functions';
+
 describe('calculator functions', () => {
-  it('multiplies', () => {
-    let result = multiply(1,2);
-    expect(result).toEqual(2);
-    result = multiply(2,2);
-    expect(result).toEqual(4);
-  });
+  describe('adds', () => {
+    test('4 and 5', () => {
+      let result = add(4,5);
+      expect(result).toEqual(9);
+    })
+  })
+  describe('subtracts', () => {
+    test('9 and 2', () => {
+      let result = subtract(9,2);
+      expect(result).toEqual(7);
+    })
+  })
+  describe('multiplies', () => {
+    test('10 and 2', () => {
+      let result = multiply(10,2);
+      expect(result).toEqual(20);
+    })
+  })
+  describe('divides', () => {
+    test('10 and 2', () => {
+      let result = divide(10,2);
+      expect(result).toEqual(5);
+    })
+  })
+})
+
+describe('sets inputs', () => {
+  describe('setOperator', () => {
+    test('division',() => {
+      let result = setOperator('division');
+      expect(result).toEqual('division');
+    })
+  })
 })
